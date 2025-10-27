8e6c64be964b00a03fa3e28ffb8184a46c68f451
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-10-27 10:17:53 -0300

    infra: Atualiza handelr request para aws http v2

--- link-shortener-app/src/main/java/dev/senna/StreamLambdaHandler.java
+++ link-shortener-app/src/main/java/dev/senna/StreamLambdaHandler.java
@@ -30,6 +30,8 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
+
         handler.proxyStream(inputStream, outputStream, context);
+
     }
 }
