067032812f711b4512cb6d286774e03d46e70c8e
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2025-10-27 09:39:42 -0300

    infra: Remove sqs

--- link-shortener-app/src/main/java/dev/senna/config/AwsConfig.java
+++ link-shortener-app/src/main/java/dev/senna/config/AwsConfig.java
@@ -1,11 +1,9 @@
 package dev.senna.config;
 
-import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
@@ -19,21 +17,6 @@ public class AwsConfig {
         return DynamoDbClient.builder()
                 .endpointOverride(URI.create(localStackUrl))
                 .region(Region.SA_EAST_1)
-                .build();
-    }
-
-    @Bean
-    public SqsAsyncClient sqsAsyncClient() {
-        return SqsAsyncClient.builder()
-                .endpointOverride(URI.create("http://localstack-main:4566"))
-                .region(Region.SA_EAST_1)
-                .build();
-    }
-
-    @Bean
-    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
-        return SqsTemplate.builder()
-                .sqsAsyncClient(sqsAsyncClient)
                 .build();
     }
 }
